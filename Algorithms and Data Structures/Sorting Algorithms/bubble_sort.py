82ff62259bd4b8bf2fbd9c6e35cbf2f55cf7ff4f
Milena <milena.marcinik@gmail.com>
2020-11-17 20:48:23 +0100

    Add the correct solution to decorator exercise

---
+++ Algorithms and Data Structures/Sorting Algorithms/bubble_sort.py
@@ -0,0 +1,24 @@
+"""
+Python program for implementation of Bubble Sort
+
+O(n^2)
+
+https://stackabuse.com/bubble-sort-in-python/
+https://www.geeksforgeeks.org/bubble-sort/
+"""
+
+def bubble_sort(array):
+    """
+    Bubble sort function.
+
+    :param array: given list
+    :return: sorted list
+    """
+    # Gets the length of the array
+    length = len(array)
+
+    for num in range(length):
+        for i in range(length - 1):
+            # Swap if the element found is greater than the next element
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
